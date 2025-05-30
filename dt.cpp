86e10118aa3891abdbc0fde2daa692058bd88077
Zhi Li <lznuaa@gmail.com>
2025-05-30 14:14:20 -0400

    Fix first line comments have extra tab issue
    Signed-off-by: Zhi Li <lznuaa@gmail.com>

--- dt.cpp
+++ dt.cpp
@@ -9,6 +9,8 @@
 #include <cctype>
 #include <locale>
 
+#define HEAD_PRO "_____HEADER____"
+
 using namespace std;
 
 inline std::string str_to_lower(const std::string &str)
@@ -215,14 +217,21 @@ class DeviceTreeNode {
 			std::cout << "\n" << indentation << name << " {";
 		}
 
-		if (!properties.empty())
+		if (!properties.empty() && indent >= 0)
 			std::cout << "\n";
 
 		for (const auto& prop : properties) {
-			std::cout << indentation << "\t" << prop.first;
-			if (prop.second.length() > 0)
-				std::cout << " = " << prop.second;
-			std::cout << ";\n";
+
+			if (indent >= 0) {
+				std::cout << indentation << "\t" << prop.first;
+
+				if (prop.second.length() > 0)
+					std::cout << " = " << prop.second;
+
+				std::cout << ";\n";
+			} else {
+				std::cout << prop.second;
+			}
 		}
 		for (const auto& child : children) {
 			child.second->print_tree(indent + 1);
@@ -258,6 +267,50 @@ class DeviceTreeParser {
 
 	~DeviceTreeParser() { delete root; }
 
+	size_t find_dt_start(std::string &s) {
+		size_t i = 0;
+		size_t last_cr = 0;
+		enum {	START_S,
+			NORMAL,
+			COMMENT_SINGLE,
+			COMMENT_MULTI,
+			COMMENT_MULTI_E, } mode = NORMAL;
+
+		for (i = 0; i < s.size(); i++) {
+			char c = s[i];
+			if (c == '\n')
+				last_cr = i + 1;
+
+			switch (mode) {
+			case NORMAL:
+				if (c == '/') mode = START_S;
+				if (c == '{') return last_cr;
+				break;
+			case START_S:
+				if (c == '/') mode = COMMENT_SINGLE;
+				if (c == '*') mode = COMMENT_MULTI;
+				break;
+			case COMMENT_SINGLE:
+				if (c == '\n') mode = NORMAL;
+				break;
+			case COMMENT_MULTI:
+				if (c == '*') mode = COMMENT_MULTI_E;
+				break;
+			case COMMENT_MULTI_E:
+				if (c == '/')
+					mode = NORMAL;
+				else
+					mode = COMMENT_MULTI;
+				break;
+			default:
+				mode = NORMAL;
+				break;
+			}
+		}
+
+		return 0;
+	}
+
 	// Parse the device tree file
 	void parse(const std::string& file_path, bool warning = false) {
 		std::ifstream file(file_path);
@@ -271,15 +324,21 @@ class DeviceTreeParser {
 				    std::istreambuf_iterator<char>());
 		file.close();
 
-		// Remove comments and trim the content
-		content = remove_comments(content);
+		// Trim the content
 		content = trim(content);
+
+		size_t start = find_dt_start(content);
 
 		DeviceTreeNode* current_node = root;
 		std::vector<DeviceTreeNode*> node_stack = {root};
 
+		size_t pos = find_dt_start(content);
+
+		if (pos > 0)
+			root -> add_property(HEAD_PRO, content.substr(0, pos - 1));
+
 		std::string buffer;
-		for (size_t i = 0; i < content.size(); ++i) {
+		for (size_t i = start; i < content.size(); ++i) {
 			char c = content[i];
 			if (c == '{') {
 				// Start of a new node
